7c250ae1a0e167d4bcad60726b0322386f347e27
Lorenzo <lore.grolla04@gmail.com>
2025-12-10 09:52:33 +0100

    modifica chiavi esterne

--- db/Database.sql
+++ db/Database.sql
@@ -17,7 +17,17 @@ CREATE TABLE Gioco (
     Immagine VARCHAR(255)
 );
 
--- Tabella Vittima
+-- Tabella Caso 
+CREATE TABLE Caso (
+    N_Caso INT PRIMARY KEY AUTO_INCREMENT,
+    Data DATE NOT NULL,
+    Luogo VARCHAR(100) NOT NULL,
+    Descrizione TEXT NOT NULL,
+    Tipologia VARCHAR(50) NOT NULL,
+    Immagine VARCHAR(255) NOT NULL
+);
+
+-- Tabella Vittima 
 CREATE TABLE Vittima (
     CF_Vittima VARCHAR(16) PRIMARY KEY,
     Nome VARCHAR(50) NOT NULL,
@@ -25,9 +35,8 @@ CREATE TABLE Vittima (
     LuogoNascita VARCHAR(100) NOT NULL,
     DataNascita DATE NOT NULL,
     DataDecesso DATE,
-    Caso VARCHAR(16) NOT NULL,
-    FOREIGN KEY (Caso) REFERENCES Caso(N_Caso)
-
+    Caso INT NOT NULL,  
+    FOREIGN KEY (Caso) REFERENCES Caso(N_Caso) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabella Colpevole
@@ -40,16 +49,6 @@ CREATE TABLE Colpevole (
     Immagine VARCHAR(255) NOT NULL
 );
 
--- Tabella Caso
-CREATE TABLE Caso (
-    N_Caso INT PRIMARY KEY AUTO_INCREMENT,
-    Data DATE NOT NULL,
-    Luogo VARCHAR(100) NOT NULL,
-    Descrizione TEXT NOT NULL,
-    Tipologia VARCHAR(50) NOT NULL,
-    Immagine VARCHAR(255) NOT NULL
-);
-
 -- Tabella Articolo
 CREATE TABLE Articolo (
     ID_Articolo INT PRIMARY KEY AUTO_INCREMENT,
@@ -57,7 +56,7 @@ CREATE TABLE Articolo (
     Data DATE NOT NULL,
     Link VARCHAR(255) NOT NULL,
     Caso INT,
-    FOREIGN KEY (Caso) REFERENCES Caso(N_Caso)
+    FOREIGN KEY (Caso) REFERENCES Caso(N_Caso) ON DELETE SET NULL ON UPDATE CASCADE
 );
 
 -- Tabella Domanda
@@ -65,8 +64,8 @@ CREATE TABLE Domanda (
     ID_Domanda INT PRIMARY KEY AUTO_INCREMENT,
     Tipologia VARCHAR(50) NOT NULL,
     Testo TEXT NOT NULL,
-    Gioco INT NOT NULL,
-    FOREIGN KEY (Gioco) REFERENCES Gioco(Nome)
+    Gioco VARCHAR(100) NOT NULL, 
+    FOREIGN KEY (Gioco) REFERENCES Gioco(Nome) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabella Risposta
@@ -75,7 +74,7 @@ CREATE TABLE Risposta (
     Opzione TEXT NOT NULL,
     IsTrue BOOLEAN NOT NULL,
     Domanda INT NOT NULL,
-    FOREIGN KEY (Domanda) REFERENCES Domanda(ID_Domanda)
+    FOREIGN KEY (Domanda) REFERENCES Domanda(ID_Domanda) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabella Commento
@@ -85,18 +84,18 @@ CREATE TABLE Commento (
     Commento TEXT NOT NULL,
     ID_Utente INT NOT NULL,
     ID_Caso INT NOT NULL,
-    FOREIGN KEY (ID_Utente) REFERENCES Utente(ID_Utente),
-    FOREIGN KEY (ID_Caso) REFERENCES Caso(N_Caso)
+    FOREIGN KEY (ID_Utente) REFERENCES Utente(ID_Utente) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (ID_Caso) REFERENCES Caso(N_Caso) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabella Partita (relazione tra Gioco e Utente)
 CREATE TABLE Partita (
     Utente INT NOT NULL,
     Gioco VARCHAR(100) NOT NULL,
-    Punteggio INT Default 0,
+    Punteggio INT DEFAULT 0,
     PRIMARY KEY (Utente, Gioco),
-    FOREIGN KEY (Utente) REFERENCES Utente(ID_Utente),
-    FOREIGN KEY (Gioco) REFERENCES Gioco(Nome)
+    FOREIGN KEY (Utente) REFERENCES Utente(ID_Utente) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Gioco) REFERENCES Gioco(Nome) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 -- Tabella Colpa (relazione tra Colpevole e Caso)
@@ -104,6 +103,6 @@ CREATE TABLE Colpa (
     Colpevole VARCHAR(16) NOT NULL,
     Caso INT NOT NULL,
     PRIMARY KEY (Colpevole, Caso),
-    FOREIGN KEY (Colpevole) REFERENCES Colpevole(CF_Colpevole),
-    FOREIGN KEY (Caso) REFERENCES Caso(N_Caso)
+    FOREIGN KEY (Colpevole) REFERENCES Colpevole(CF_Colpevole) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (Caso) REFERENCES Caso(N_Caso) ON DELETE CASCADE ON UPDATE CASCADE
 );
